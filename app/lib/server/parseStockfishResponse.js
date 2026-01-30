e509c2321bbaa8d122c516773cd20bd70e98a30f
weterynarzfred <weterynarzfred@gmail.com>
2026-01-30 10:22:21 +0100

    remove computation time from cache

--- app/lib/server/parseStockfishResponse.js
+++ app/lib/server/parseStockfishResponse.js
@@ -49,7 +49,6 @@ export default function parseStockfishResponse(data, startTime, fen) {
         result = {
           bestMove,
           eval: evalCp,
-          timeMs: Date.now() - startTime,
         };
       }
     });
